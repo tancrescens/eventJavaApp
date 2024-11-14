8be66cf20fd3c1e85f9eb52a5db7caa851fa2c82
Crescens <2e3crescens@gmail.com>
2024-11-14 14:48:28 +0800

    edited Attendee constructor

--- Attendee.java
+++ Attendee.java
@@ -8,7 +8,8 @@ public class Attendee extends Person {
         this.groupNo = 0;
     }
 
-    public Attendee(Integer groupNo) {
+    public Attendee(String firstname, String lastname, String icNo, Integer groupNo) {
+        super(firstname, lastname, icNo);
         this.groupNo = groupNo;
     }
 
